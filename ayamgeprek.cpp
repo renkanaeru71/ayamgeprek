7efa2231a5301e981ab4aab0bbd4d2fc37ed2393
renkanaeru71 <158247113+renkanaeru71@users.noreply.github.com>
2025-11-08 16:47:02 +0700

    akunakayamgeprekkk
    laparrrrrrrrrrrrrrrrrrr

---
+++ ayamgeprek.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string makan;
+    cout << "Hello, Ayam Geprek!" << endl;
+    cout << "siapa yang mau ayam geprek?" << endl;
+    cin >> makan;
+    cout << "Selamat menikmati ayam geprek, " << makan << "!" << endl;
+    return 0;
+}
